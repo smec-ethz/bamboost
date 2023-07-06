fe96d6f9e6748e7f7668ec653632ef729cc21ba1
florez <florez@ethz.ch>
2023-07-06 10:01:41 +0200

    add setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbmanager',
+    version='0.1',
+    description='A light weight database manager using HDF5',
+    url='https://gitlab.ethz.ch/compmechmat/research/libs/dbmanager',
+    author='florez',
+    author_email='florez@ethz.ch',
+    license='MIT',
+    packages=find_packages(include=['dbmanager', 'dbmanager.*']),
+    install_requires=[
+        'h5py',
+        'pandas',
+        'argparse',
+        'mpi4py',
+        'numpy',
+        ],
+)
