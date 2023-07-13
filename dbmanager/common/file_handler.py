b0277f9055446df8b99d47e5565af459b2f5ce81
florez <florez@ethz.ch>
2023-07-13 15:52:51 +0200

    solution for keeping file open, some bugs, should write virtual dataset to other file

--- dbmanager/common/file_handler.py
+++ dbmanager/common/file_handler.py
@@ -1,4 +1,6 @@
+from itertools import cycle
 import time
+import sys
 import h5py
 
 
@@ -11,9 +13,9 @@ def open_h5file(file: str, mode, driver=None, comm=None):
         driver (str): driver for h5.File
         comm: MPI communicator
     """
+    dots = cycle(['.','..','...'])
     while True:
         try:
-
             try:
                 if driver=='mpio' and h5py._MPI_ACTIVE:
                     return h5py.File(file, mode, driver=driver, comm=comm)
@@ -23,5 +25,6 @@ def open_h5file(file: str, mode, driver=None, comm=None):
                 return h5py.File(file, mode)
 
         except OSError:
+            print(f"File {file} not accessible, waiting{next(dots)}", flush=True, end="\r") 
             time.sleep(1)
-            print(f'File {file} not accessible, waiting...', end='\r') 
+            sys.stdout.write("\033[K")
--- dbmanager/simulation.py
+++ dbmanager/simulation.py
@@ -26,10 +26,13 @@ def temporary_open_file(mode: str = 'r'):
         @wraps(method)
         def wrapper(self, *args, **kwargs):
             if self.opts['dataset.ram']:
-                with self.open(mode) as self._file:
+                with self.open(mode):
                     return method(self, *args, **kwargs)
             else:
-                assert self._file, "If `dataset.ram` is ON you need to manually open/close the file"
+                assert self._file, "If `dataset.ram` is OFF you need to manually open/close the file"
+                if self._file.mode!=mode:
+                    self.close()
+                    self.open(mode)
                 return method(self, *args, **kwargs)
         return wrapper
     return decorator
@@ -63,7 +66,7 @@ class Simulation:
         self.prank = self.comm.rank
         self.ranks = np.array([i for i in range(self.psize)])
 
-    def open(self, mode: str = 'r') -> h5py.File:
+    def open(self, mode: str = 'r', driver=None, comm=None) -> h5py.File:
         """Open the HDF5 file.
 
         Args:
@@ -71,7 +74,12 @@ class Simulation:
         Returns:
             h5py file object
         """
-        self._file = open_h5file(self.h5file, mode=mode)
+        if self._file:
+            if self._file.mode==mode:
+                return self._file
+            self.close()
+
+        self._file = open_h5file(self.h5file, mode=mode, driver=driver, comm=comm)
         return self._file
 
     def close(self) -> None:
@@ -83,7 +91,7 @@ class Simulation:
     def parameters(self):
         tmp_dict = dict()
         if self.prank==0:
-            with open_h5file(self.h5file, 'r') as file:
+            with self.open('r') as file:
                 tmp_dict.update(file['parameters'].attrs)
                 for key in file['parameters'].keys():
                     tmp_dict.update({key: file[f'parameters/{key}'][()]})
@@ -108,7 +116,7 @@ class Simulation:
         Returns:
             SimpleNamespace with all data.
         """
-        with open_h5file(self.h5file, 'r') as f:
+        with self.open('r') as f:
             data = f['postprocess']
             d = {key: data[key][()] for key in data.keys()}
         return SimpleNamespace(**d)
@@ -119,7 +127,7 @@ class Simulation:
         Args:
             update_dict: dictionary to push
         """
-        with open_h5file(self.h5file, 'a') as file:
+        with self.open('a') as file:
             file.attrs.update(update_dict)
 
     def update_parameters(self, update_dict: dict) -> None:
@@ -129,7 +137,7 @@ class Simulation:
             update_dict: dictionary to push
         """
         if self.prank==0:
-            with open_h5file(self.h5file, 'a') as file:
+            with self.open('a') as file:
                 file['parameters'].attrs.update(update_dict)
 
     def create_xdmf_file(self, fields: list = None, nb_steps: int = None) -> None:
@@ -142,7 +150,7 @@ class Simulation:
         """
         if not nb_steps:
             if self.comm.rank==0:
-                with open_h5file(self.h5file, 'r') as f:
+                with self.open('r') as f:
                     grp_name = list(f['data'].keys())[0]
                     nb_steps = list(f[f'data/{grp_name}'].keys())
                     nb_steps = max([int(step) for step in nb_steps])
@@ -150,7 +158,7 @@ class Simulation:
         if self.comm.rank==0:
 
             if not fields:
-                with open_h5file(self.h5file, 'r') as f:
+                with self.open('r') as f:
                     fields = list(f['data'].keys())
 
             xdmf_writer = XDMFWriter(self.xdmffile, self.h5file)
@@ -191,7 +199,7 @@ class Simulation:
         else:
             job.create_bash_script_local(commands, path=os.path.abspath(self.parent_path),
                                          uid=self.uid, ntasks=ntasks)
-        with open_h5file(self.h5file, 'a') as file:
+        with self.open('a') as file:
             file.attrs.update({'submitted': False})
 
     def submit(self) -> None:
@@ -200,20 +208,20 @@ class Simulation:
         subprocess.Popen(["sbatch", f"{batch_script}"])
         print(f'Simulation {self.uid} submitted!')
 
-        with open_h5file(self.h5file, 'a') as file:
+        with self.open('a') as file:
             file.attrs.update({'submitted': True})
 
+    @temporary_open_file('a')
     def change_note(self, note) -> None:
-        with open_h5file(self.h5file, 'a') as file:
-            file.attrs['notes'] = note
+        self._file.attrs['notes'] = note
 
+    @temporary_open_file('a')
     def add_postprocess_field(self, name: str, vector: np.array) -> None:
         """Add a custom field in postprocessing."""
-        with open_h5file(self.h5file, 'a') as f:
-            data = f.require_group('postprocess')  # Require group data to store all point data in
-            if name in data.keys():
-                del data[name]
-            data.create_dataset(name, data=vector)
+        data = self._file.require_group('postprocess')  # Require group data to store all point data in
+        if name in data.keys():
+            del data[name]
+        data.create_dataset(name, data=vector)
 
 
 class SimulationReader(Simulation):
@@ -234,38 +242,38 @@ class SimulationReader(Simulation):
                     print(f"Dataset: {name}")
             file.visititems(print_hdf5_item)
 
+    @temporary_open_file('r')
     def access(self, path: str):
         """Simply access data in hdf5 file tree"""
-        with open_h5file(self.h5file, 'r') as file:
-            return file[f'{path}'][()]
+        return self._dataset(self._file[f'{path}'])
 
     @property
+    @temporary_open_file('r')
     def mesh(self):
         """Return coordinates and connectivity."""
-        with open_h5file(self.h5file, 'r') as file:
-            coordinates = file[f'{self.mesh_location}/geometry'][()]
-            cells = file[f'{self.mesh_location}/topology'][()]
-            return coordinates, cells
+        coordinates = self._dataset(self._file[f'{self.mesh_location}/geometry'])
+        cells = self._dataset(self._file[f'{self.mesh_location}/topology'])
+        return coordinates, cells
 
     @property
+    @temporary_open_file('r')
     def globals(self):
         """Return globals."""
-        with open_h5file(self.h5file, 'r') as file:
-            grp = file['globals']
-            d = {key: grp[key][()] for key in grp.keys()}
-            return pd.DataFrame.from_dict(d)
+        grp = self._file['globals']
+        d = {key: grp[key][()] for key in grp.keys()}
+        return pd.DataFrame.from_dict(d)
 
     @property
+    @temporary_open_file('r')
     def data_info(self) -> pd.Dataframe:
         """View the data stored."""
         tmp_dictionary = dict()
-        with open_h5file(self.h5file, 'r') as file:
-            for data in file['data'].keys():
-                steps = max([int(step) for step in file[f'data/{data}'].keys()]) + 1
-                shape = file[f'data/{data}/0'].shape
-                dtype = file[f'data/{data}/0'].dtype
-                tmp_dictionary[data] = {'dtype': dtype, 'shape': shape, 'steps': steps}
-            return pd.DataFrame.from_dict(tmp_dictionary)
+        for data in self._file['data'].keys():
+            steps = max([int(step) for step in self._file[f'data/{data}'].keys()]) + 1
+            shape = self._file[f'data/{data}/0'].shape
+            dtype = self._file[f'data/{data}/0'].dtype
+            tmp_dictionary[data] = {'dtype': dtype, 'shape': shape, 'steps': steps}
+        return pd.DataFrame.from_dict(tmp_dictionary)
 
     @property
     def git(self) -> str:
@@ -333,14 +341,11 @@ class SimulationReader(Simulation):
             return ds[()]
         return ds
 
+    @temporary_open_file('r+')
     def _create_vds(self, name, length) -> None:
         """Create virtual dataset of the full timeseries of a field.
         Requires HDF5 > 1.10 !
         """
-        # Close and reopen with writing rights
-        self.close()
-        self.open('r+')
-
         grp = self._file[f'data/{name}']
         shape_i = grp['0'].shape
         dtype_i = grp['0'].dtype
