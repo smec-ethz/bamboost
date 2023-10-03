b4e33292f9e774c61f5b05cadb062d027437a955
florez <florez@ethz.ch>
2023-10-03 18:39:57 +0200

    remove obsolete content in setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='bamboost',
-    version='0.3',
-    description='A light weight database manager using HDF5',
-    url='https://gitlab.ethz.ch/compmechmat/research/libs/dbmanager',
-    author='florez',
-    author_email='florez@ethz.ch',
-    license='MIT',
-    packages=find_packages(include=['bamboost', 'bamboost.*']),
-    install_requires=[
-        'pandas',
-        'mpi4py',
-        'numpy',
-        ],
-    # h5py is not in the install requirements because it would reinstall
-    # messing with the installation for parallel support :/
-)
+setup()
