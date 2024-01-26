eab83eb91431f71f85c8d14c47ba38e7952e3db6
sannant <asanner@ethz.ch>
2024-01-26 16:23:30 +0100

    ENH: userdata datasets infer or impose dtype. Closes #21

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -57,8 +57,9 @@ def test_field_dtype_inferred(temp_manager, array):
     # The dtypes are equivalent (although they may have another string representation)
     assert np.dtype(sim.data["test"].dtype) == array.dtype
 
-@pytest.mark.parametrize("val", [1.,1,])
-def test_field_dtype_inferred(temp_manager, val):
+
+@pytest.mark.parametrize("val", [1., 1, ])
+def test_global_field_dtype_inferred(temp_manager, val):
     sim = temp_manager.create_simulation()
     uid = sim.uid
     sim.add_global_field("test", val)
@@ -72,3 +73,40 @@ def test_field_dtype_inferred(temp_manager, val):
 
     # The dtypes are equivalent (although they may have another string representation)
     assert np.dtype(sim.globals["test"].dtype) == np.array([val]).dtype
+
+
+@pytest.mark.parametrize("array", [np.array([1., 2.]),
+                                   np.array([1, 2], dtype="int"),
+                                   ])
+def test_userdata_dtype_inferred(temp_manager, array):
+    sim = temp_manager.create_simulation()
+    uid = sim.uid
+    sim.userdata.add_dataset("test", array)
+
+    sim.finish_step()
+    sim.userdata.add_dataset("test", 2 * array)
+
+    sim.finish_sim()
+
+    sim = temp_manager.sim(uid)
+
+    # The dtypes are equivalent (although they may have another string representation)
+    assert np.dtype(sim.userdata["test"].dtype) == array.dtype
+
+
+@pytest.mark.parametrize("dtype,array", [['<f4', np.array([1, 2], dtype=int)],
+                                         ["int32", np.array([1, 2])],
+                                         ])
+def test_userdata_dtype_forced(temp_manager, dtype, array):
+    sim = temp_manager.create_simulation()
+    uid = sim.uid
+    sim.userdata.add_dataset("test", array, dtype=dtype)
+
+    sim.finish_step()
+    sim.userdata.add_dataset("test", 2 * array, dtype=dtype)
+
+    sim.finish_sim()
+
+    sim = temp_manager.sim(uid)
+
+    assert sim.userdata["test"].dtype == dtype
