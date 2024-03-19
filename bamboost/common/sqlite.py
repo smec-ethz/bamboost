13ef4ffcad0efe84fb7cb1f43c125ac5596f034a
florez <florez@ethz.ch>
2024-03-19 01:56:02 +0100

    WIP: refactor index, make index a database sqlite

---
+++ bamboost/common/sqlite.py
@@ -0,0 +1,161 @@
+# This file is part of bamboost, a Python library built for datamanagement
+# using the HDF5 file format.
+#
+# https://gitlab.ethz.ch/compmechmat/research/libs/dbmanager
+#
+# Copyright 2023 Flavio Lorez and contributors
+#
+# There is no warranty for this code
+from __future__ import annotations
+from functools import wraps
+
+import json
+import sqlite3
+from contextlib import contextmanager
+from typing import Generator
+from typing_extensions import Self
+import logging
+
+import numpy as np
+
+from bamboost.common.mpi import MPI
+
+log = logging.getLogger(__name__)
+
+DATA_TYPES = {
+    "ARRAY": "ARRAY",
+    "JSON": "JSON",
+    np.datetime64: "DATETIME",
+    int: "INTEGER",
+    float: "REAL",
+    str: "TEXT",
+    bool: "BOOL",
+}
+
+
+def parse_sqlite_type(val):
+    if isinstance(val, np.ndarray):
+        return "ARRAY"
+    if isinstance(val, np.generic):
+        return DATA_TYPES.get(type(val.item()), "TEXT")
+    if isinstance(val, dict):
+        return "JSON"
+    return DATA_TYPES.get(type(val), "TEXT")
+
+
+SYNC_TABLE = True
+
+
+# Converts np.array to TEXT when inserting
+sqlite3.register_adapter(np.ndarray, lambda arr: json.dumps(arr.tolist()))
+
+adapt_numpy_number = lambda val: val.item()
+sqlite3.register_adapter(np.int_, adapt_numpy_number)
+sqlite3.register_adapter(np.float_, adapt_numpy_number)
+sqlite3.register_adapter(np.datetime64, adapt_numpy_number)
+sqlite3.register_adapter(bool, lambda val: int(val))
+
+# Converts TEXT to np.array when selecting
+sqlite3.register_converter("ARRAY", lambda text: np.array(json.loads(text)))
+sqlite3.register_converter("JSON", lambda text: json.loads(text))
+sqlite3.register_converter("BOOL", lambda val: bool(val))
+
+# ----------------
+# DECORATORS
+# ----------------
+def with_connection(func):
+    """Decorator to ensure that the cursor is available. If the cursor is not available, the
+    connection is opened and closed after the function is executed."""
+
+    @wraps(func)
+    def wrapper(self: SQLiteDatabase, *args, **kwargs):
+        cursor: sqlite3.Cursor = self._cursor
+        # check if cursor is available
+        if not self._is_open or cursor is None:
+            with self.open():
+                return func(self, *args, **kwargs)
+        return func(self, *args, **kwargs)
+
+    return wrapper
+
+
+def on_rank_0(func):
+    """Decorator to ensure that the function is only executed on rank 0."""
+
+    @wraps(func)
+    def wrapper(self: SQLiteDatabase, *args, **kwargs):
+        if self._comm.rank == 0:
+            return func(self, *args, **kwargs)
+        return None
+
+    return wrapper
+
+class SQLiteDatabase:
+    """
+    Class to handle sqlite databases.
+    """
+
+    def __init__(self, _comm = MPI.COMM_WORLD) -> None:
+        if _comm.rank != 0:
+            return
+        self._comm = _comm
+        self._conn = None
+        self._cursor = None
+        self._is_open = False
+        self._lock_stack = 0
+
+    @property
+    def _lock_stack(self) -> int:
+        return self.__lock_stack
+
+    @_lock_stack.setter
+    def _lock_stack(self, value: int) -> None:
+        self.__lock_stack = value if value >= 0 else 0
+
+    def connect(self) -> Self:
+        self._lock_stack += 1
+        if not self._is_open:
+            log.debug(f"Connecting to {self.file}")
+            self._conn = sqlite3.connect(
+                self.file, detect_types=sqlite3.PARSE_DECLTYPES
+            )
+            self._cursor = self._conn.cursor()
+            self._is_open = True
+        return self
+
+    def close(self, *, force: bool = False, ensure_commit: bool = False) -> Self:
+        if force:
+            self._lock_stack = 0
+        else:
+            self._lock_stack -= 1
+
+        if self._lock_stack <= 0 and self._is_open:
+            log.debug(f"Closing connection to {self.file}")
+            self._conn.commit()
+            self._conn.close()
+            self._is_open = False
+            return self
+
+        if ensure_commit:
+            self._conn.commit()
+
+        return self
+
+    def commit(self) -> None:
+        self._conn.commit()
+        return self
+
+    @contextmanager
+    def open(self) -> Generator[Self]:
+        """The open method is used as a context manager.
+
+        Args:
+            ensure_commit (bool, optional): Ensure that the connection is committed. Defaults to False.
+
+        Example:
+            >>> with index.open() as table:
+            >>>     table._cursor.execute("SELECT * FROM database")
+        """
+        self.connect()
+        yield self
+        self.close()
--- bamboost/index.py
+++ bamboost/index.py
@@ -9,23 +9,53 @@
 """Module to manage the database index and its ID's."""
 
 from __future__ import annotations
+from time import time
+
+__all__ = [
+    "Index",
+    "DatabaseTable",
+    "Entry",
+    "record_database",
+    "get_path",
+    "find",
+    "clean",
+    "create_index",
+    "get_uid_from_path",
+    "get_index_dict",
+    "get_known_paths",
+    "get_uid_from_path",
+]
 
 import json
 import logging
 import os
+import sqlite3
 import subprocess
+from contextlib import contextmanager
+from dataclasses import dataclass
+from typing import Generator
+
+import numpy as np
+import pandas as pd
+
+from bamboost.common.file_handler import open_h5file
+
+from .common.sqlite import SQLiteDatabase, parse_sqlite_type, with_connection
 
 log = logging.getLogger(__name__)
 
 
+# ------------------
 # Define directories
 # ------------------
 
 HOME = os.path.expanduser("~")
 CONFIG_DIR = os.path.join(HOME, ".config", "bamboost")
+LOCAL_DIR = os.path.join(HOME, ".local", "share", "bamboost")
 DATABASE_INDEX = os.path.join(CONFIG_DIR, "database_index.json")
 KNOWN_PATHS = os.path.join(CONFIG_DIR, "known_paths.json")
 PREFIX = ".BAMBOOST-"
+DOT_REPLACEMENT = "DOT"
 
 # Create config files if they don't exist
 os.makedirs(CONFIG_DIR, exist_ok=True)
@@ -36,70 +66,320 @@ if not os.path.isfile(KNOWN_PATHS):
     with open(KNOWN_PATHS, "w") as file:
         file.write(json.dumps([], indent=4))
 
+# Create local directory if it doesn't exist
+os.makedirs(LOCAL_DIR, exist_ok=True)
 
+
+# ------------------
+# Class definitions
+# ------------------
+class Singleton(type):
+    """Singleton metaclass to create a single instance of the database."""
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Index(SQLiteDatabase, metaclass=Singleton):
+    """
+    SQLite database to store database ID, path lookup. As well as the table for
+    each database. Location: ~/.local/share/bamboost
+    Singleton pattern.
+
+    Attributes:
+        file (str): path to the database file
+        _conn (sqlite3.Connection): connection to the database
+        _cursor (sqlite3.Cursor): cursor for the database
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.file = os.path.join(LOCAL_DIR, "database.db")
+        self.create_index_table()
+
+    @with_connection
+    def create_index_table(self) -> None:
+        """Create the index table if it does not exist."""
+        self._cursor.execute(
+            """CREATE TABLE IF NOT EXISTS dbindex (id TEXT PRIMARY KEY, path TEXT)"""
+        )
+
+    @with_connection
+    def read_table(self, *args, **kwargs) -> pd.DataFrame:
+        """Read the index table.
+
+        Returns:
+            pd.DataFrame: index table
+        """
+        return pd.read_sql_query("SELECT * FROM dbindex", self._conn, *args, **kwargs)
+
+    @with_connection
+    def get_path(self, id: str) -> str:
+        """Get the path of a database from its ID.
+
+        Args:
+            id (str): ID of the database
+
+        Returns:
+            str: path of the database
+        """
+        self._cursor.execute("SELECT path FROM dbindex WHERE id=?", (id,))
+        path_db = self._cursor.fetchone()[0]
+        if _check_path(id, path_db):
+            return path_db
+
+        # if path is wrong, try to find it
+        for root_dir in get_known_paths():
+            res = find(id, root_dir)
+            if res:
+                path = os.path.dirname(res[0])
+                self.insert_path(id, path)
+                return path
+
+        # last resort, check home
+        res = find(id, HOME)
+        if res:
+            path = os.path.dirname(res[0])
+            self.insert_path(id, path)
+            return path
+
+        raise FileNotFoundError(f"Database {id} not found on system.")
+
+    @with_connection
+    def insert_path(self, id: str, path: str) -> None:
+        """Insert a database path into the index.
+
+        Args:
+            id (str): ID of the database
+            path (str): path of the database
+        """
+        self._cursor.execute("INSERT OR REPLACE INTO dbindex VALUES (?, ?)", (id, path))
+
+    @with_connection
+    def get_id(self, path: str) -> str:
+        """Get the ID of a database from its path.
+
+        Args:
+            path (str): path of the database
+
+        Returns:
+            str: ID of the database
+        """
+        self._cursor.execute("SELECT id FROM dbindex WHERE path=?", (path,))
+        return self._cursor.fetchone()[0]
+
+    @with_connection
+    def scan_known_paths(self) -> dict:
+        """Scan known paths for databases and update the index."""
+        for path in get_known_paths():
+            completed_process = subprocess.run(
+                ["find", path, "-iname", f"{PREFIX}*", "-not", "-path", "*/\.git/*"],
+                capture_output=True,
+            )
+            databases_found = completed_process.stdout.decode("utf-8").splitlines()
+            for database in databases_found:
+                name = os.path.basename(database)
+                id = name.split("-")[1]
+                self.insert_path(id, os.path.dirname(database))
+
+
+class DatabaseTable:
+    """
+    Class to manage the table of a database. Multiton pattern. One table per
+    database.
+    """
+
+    _instances = {}
+
+    def __new__(cls, id: str, *_) -> DatabaseTable:
+        if id not in cls._instances:
+            cls._instances[id] = super().__new__(cls)
+        return cls._instances[id]
+
+    def __init__(self, id: str, _index: Index):
+        if hasattr(self, "_initialized"):
+            return
+
+        self.id = id
+        self._index = _index
+        self._entries = {}
+        self._initialized = True
+        self.path = self._index.get_path(self.id)
+        self.update_times_table = f"{self.id}_update_times"
+        self.create_database_table()
+
+    def __getattr__(self, name):
+        if name in {
+            "_conn",
+            "_cursor",
+            "open",
+            "close",
+            "commit",
+            "_is_open",
+        }:
+            return getattr(self._index, name)
+        return self.__getattribute__(name)
+
+    # ---------------------
+    # Database table functions
+    # ---------------------
+    @with_connection
+    def read_table(self) -> pd.DataFrame:
+        """Read the table of the database.
+
+        Returns:
+            pd.DataFrame: table of the database
+        """
+        df = pd.read_sql_query(f"SELECT * FROM {self.id}", self._conn)
+        df.rename(columns=lambda x: x.replace(DOT_REPLACEMENT, "."), inplace=True)
+        return df
+
+    @with_connection
+    def drop_table(self) -> None:
+        """Drop the table of the database."""
+        self._cursor.execute(f"DROP TABLE {self.id}")
+        self._cursor.execute(f"DROP TABLE {self.update_times_table}")
+
+    @with_connection
+    def create_database_table(self) -> None:
+        """Create a table for a database."""
+        self._cursor.execute(
+            f"""CREATE TABLE IF NOT EXISTS {self.id} (id TEXT PRIMARY KEY,
+                                                 time_stamp DATETIME, notes
+                                                 TEXT processors INTEGER)"""
+        )
+        self._cursor.execute(
+            f"""CREATE TABLE IF NOT EXISTS {self.update_times_table} (id TEXT PRIMARY KEY,
+                update_time DATETIME)
+            """
+        )
+
+    @with_connection
+    def update_entry(self, entry_id: str, data: dict) -> None:
+        """Update an entry in the database.
+
+        Args:
+            entry_id (str): ID of the entry
+            data (dict): data to update
+        """
+        # get columns of table
+        self._cursor.execute(f"PRAGMA table_info({self.id})")
+        cols = self._cursor.fetchall()
+
+        # check if columns exist
+        for key, val in data.items():
+            key = key.replace(".", DOT_REPLACEMENT)
+            if any(key == column[1] for column in cols):
+                continue
+            dtype = parse_sqlite_type(val)
+            self._cursor.execute(f"ALTER TABLE {self.id} ADD COLUMN {key} {dtype}")
+
+        # insert data into table
+        data.pop("id", None)
+        sql = f"""
+        INSERT INTO {self.id} (id, {", ".join(data.keys())})
+        VALUES (:id, {", ".join([f":{key}" for key in data.keys()])})
+        ON CONFLICT(id) DO UPDATE SET
+        {", ".join(f"{key} = excluded.{key}" for key in data.keys())}
+        """
+        data["id"] = entry_id
+        self._cursor.execute(sql, data)
+
+        # update update time
+        self._cursor.execute(
+            f"INSERT OR REPLACE INTO {self.update_times_table} VALUES (?, ?)",
+            (entry_id, time()),
+        )
+
+    @with_connection
+    def sync(self) -> None:
+        """Sync the table with the file system."""
+        all_ids_fs = set(
+            [
+                i
+                for i in os.listdir(self.path)
+                if os.path.isdir(os.path.join(self.path, i))
+            ]
+        )
+
+        self._cursor.execute(f"SELECT id, update_time FROM {self.update_times_table}")
+
+        for id, last_up_time in self._cursor.fetchall():
+            # remove entries that do not exist on the file system
+            if id not in all_ids_fs:
+                self._cursor.execute(f"DELETE FROM {self.id} WHERE id=?", (id,))
+                self._cursor.execute(
+                    f"DELETE FROM {self.update_times_table} WHERE id=?", (id,)
+                )
+                continue
+
+            # update entries that have been modified
+            all_ids_fs.remove(id)
+            if self.entry(id).mtime > last_up_time:
+                self.update_entry(id, self.entry(id).get_all_metadata())
+
+        # add new entries
+        for id in all_ids_fs:
+            self.update_entry(id, self.entry(id).get_all_metadata())
+
+    @with_connection
+    def entry(self, entry_id: str) -> Entry:
+        """Get the Entry object of an entry.
+        Multiton pattern. One Entry per entry.
+        """
+        if entry_id not in self._entries:
+            self._entries[entry_id] = Entry(entry_id, self.path)
+        return self._entries[entry_id]
+
+
+@dataclass
+class Entry:
+    """Simulation entry in a database.
+    Simplified version of the Simulation class in the simulation module.
+    """
+
+    def __init__(self, id: str, path: str) -> None:
+        self.id = id
+        self.path = path
+        self.h5file = os.path.join(self.path, self.id, f"{self.id}.h5")
+
+    @property
+    def metadata(self) -> dict:
+        """Get the metadata of the entry."""
+        with open_h5file(self.h5file, "r") as file:
+            return dict(file.attrs)
+
+    @property
+    def parameters(self) -> dict:
+        """Get the parameters of the entry."""
+        tmp_dict = dict()
+        with open_h5file(self.h5file, "r") as file:
+            try:
+                tmp_dict.update(file["parameters"].attrs)
+                for key in file["parameters"].keys():
+                    tmp_dict.update({key: file[f"parameters/{key}"][()]})
+            except KeyError:
+                pass
+
+        return tmp_dict
+
+    def get_all_metadata(self) -> dict:
+        """Get all metadata of the entry."""
+        return {**self.metadata, **self.parameters}
+
+    @property
+    def mtime(self) -> float:
+        """Get the modification time of the entry."""
+        return os.path.getmtime(self.h5file)
+
+
+# ----------------------
 # Module level functions
 # ----------------------
-
-
-def get_index_dict() -> dict:
-    """Returns a dictionary of all known databases."""
-    with open(DATABASE_INDEX, "r") as file:
-        try:
-            return json.loads(file.read())
-        except json.JSONDecodeError:
-            return {}
-
-
-def _write_index_dict(index: dict) -> None:
-    """Write the database index."""
-    with open(DATABASE_INDEX, "w+") as file:
-        file.write(json.dumps(index, indent=4))
-
-
-def get_known_paths() -> list:
-    with open(KNOWN_PATHS, "r") as file:
-        return json.loads(file.read())
-
-
-def _find_posix(uid, root_dir) -> list:
-    """Find function using system `find` on linux."""
-    completed_process = subprocess.run(
-        ["find", root_dir, "-iname", uid2(uid), "-not", "-path", "*/\.git/*"],
-        capture_output=True,
-    )
-    paths_found = completed_process.stdout.decode("utf-8").splitlines()
-    return paths_found
-
-
-def _find_python(uid, root_dir) -> list:
-    """Some find function for Windows or other if `find` is not working.
-
-    TODO: to be implemented
-    """
-    pass
-
-
-def uid2(uid) -> str:
-    return f"{PREFIX}{uid}"
-
-
-def get_uid_from_path(path: str) -> str:
-    """Returns the UID found in the specified path."""
-    for file in os.listdir(path):
-        if file.startswith(".BAMBOOST"):
-            return file.split("-")[1]
-    raise FileNotFoundError("No UID file found at specified path.")
-
-
-def _check_path(uid: str, path: str) -> bool:
-    """Check if path is going to the correct database"""
-    if not os.path.exists(path):
-        return False
-    if f"{PREFIX}{uid}" in os.listdir(path):
-        return True
-    return False
-
-
 def record_database(uid: str, path: str) -> None:
     """Record a database in `database_index.json`
 
@@ -193,3 +473,62 @@ def get_uid_from_path(path: str) -> str:
         if file.startswith(".BAMBOOST"):
             return file.split("-")[1]
     raise FileNotFoundError("No UID file found at specified path.")
+
+
+def get_index_dict() -> dict:
+    """Returns a dictionary of all known databases."""
+    with open(DATABASE_INDEX, "r") as file:
+        try:
+            return json.loads(file.read())
+        except json.JSONDecodeError:
+            return {}
+
+
+def get_known_paths() -> list:
+    with open(KNOWN_PATHS, "r") as file:
+        return json.loads(file.read())
+
+
+def get_uid_from_path(path: str) -> str:
+    """Returns the UID found in the specified path."""
+    for file in os.listdir(path):
+        if file.startswith(".BAMBOOST"):
+            return file.split("-")[1]
+    raise FileNotFoundError("No UID file found at specified path.")
+
+
+def _write_index_dict(index: dict) -> None:
+    """Write the database index."""
+    with open(DATABASE_INDEX, "w+") as file:
+        file.write(json.dumps(index, indent=4))
+
+
+def _find_posix(uid, root_dir) -> list:
+    """Find function using system `find` on linux."""
+    completed_process = subprocess.run(
+        ["find", root_dir, "-iname", uid2(uid), "-not", "-path", "*/\.git/*"],
+        capture_output=True,
+    )
+    paths_found = completed_process.stdout.decode("utf-8").splitlines()
+    return paths_found
+
+
+def _find_python(uid, root_dir) -> list:
+    """Some find function for Windows or other if `find` is not working.
+
+    TODO: to be implemented
+    """
+    pass
+
+
+def uid2(uid) -> str:
+    return f"{PREFIX}{uid}"
+
+
+def _check_path(uid: str, path: str) -> bool:
+    """Check if path is going to the correct database"""
+    if not os.path.exists(path):
+        return False
+    if f"{PREFIX}{uid}" in os.listdir(path):
+        return True
+    return False
