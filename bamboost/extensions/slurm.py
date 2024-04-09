65f3bd470136d87cba4780d20639e1369f722bbf
Flavio Lorez <florez@ethz.ch>
2024-04-10 01:17:29 +0200

    FEAT: slurm extension dumps all job info into attributes of '/slurm' group

--- bamboost/extensions/slurm.py
+++ bamboost/extensions/slurm.py
@@ -18,7 +18,7 @@ from bamboost.simulation_writer import SimulationWriter
 
 def _extend_exit_slurm_info(original_exit):
     @wraps(original_exit)
-    def modified_exit(self, exc_type, exc_value, exc_tb):
+    def modified_exit(self: SimulationWriter, exc_type, exc_value, exc_tb):
         # Inject the following into the __exit__ method of SimulationWriter
         slurm_job_id = os.environ.get("SLURM_JOB_ID")
 
@@ -38,11 +38,26 @@ def _extend_exit_slurm_info(original_exit):
                     key, value = line.split(":", 1)  # Split on the first colon
                     slurm_dict[key.strip()] = value.strip()
 
-            self.update_metadata(slurm_dict)
+            _write_slurm_info(self, slurm_dict)
 
         return original_exit(self, exc_type, exc_value, exc_tb)
 
     return modified_exit
+
+
+def _write_slurm_info(self: SimulationWriter, slurm_dict: dict):
+    """Write the slurm metadata to the HDF5 file.
+
+    Args:
+        - self: The SimulationWriter instance.
+        - slurm_dict: A dictionary containing the key-value pairs extracted
+          from the SLURM job.
+    """
+    # Create a group for the SLURM metadata
+    if self._prank == 0:
+        with self.open("a") as file:
+            slurm_group = file.file_object.require_group("slurm")
+            slurm_group.attrs.update(slurm_dict)
 
 
 def install():
@@ -50,3 +65,4 @@ def install():
     __exit__ method to add slurm metadata.
     """
     SimulationWriter.__exit__ = _extend_exit_slurm_info(SimulationWriter.__exit__)
+
