1b5384989b3445ce95842066af5cef79c6d441a7
florez <florez@ethz.ch>
2024-09-02 10:56:19 +0200

    chore: switch to dynamic versioning from git metadata

--- bamboost/__init__.py
+++ bamboost/__init__.py
@@ -1,10 +1,19 @@
 __author__ = "florez@ethz.ch"
 __copyright__ = ""
 __license__ = "LGPLv3"
-__version__ = "0.7.2"
 
 import logging
 from typing import Literal
+
+# Determine the version of the package
+try:
+    # If the package is installed, the version is stored in _version.py
+    from bamboost._version import __version__
+except ImportError:
+    # This is necessary if the package is not installed
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
 
 
 def add_stream_handler(logger: logging.Logger) -> None:
