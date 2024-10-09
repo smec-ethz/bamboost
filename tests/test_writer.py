9a83b380c83fa39b0c5120d510690e15667df340
florez <florez@ethz.ch>
2024-10-09 16:58:21 +0200

    test: add test

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1,4 +1,5 @@
 import os
+from unittest.mock import MagicMock
 
 import numpy as np
 import pytest
@@ -156,3 +157,52 @@ def test_enter_path(temp_manager):
             raise ValueError()
     except ValueError:
         assert os.getcwd() == original_path
+
+
+@pytest.mark.parametrize(
+    "array",
+    [
+        np.array([1, 2, 3]),
+        np.array([[1, 2, 3], [2, 3, 4]]),
+        np.array([1]),
+        np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]]),
+    ],
+)
+def test_add_field_dimensions(temp_manager, array):
+    sim = temp_manager.create_simulation()
+    uid = sim.uid
+    sim.add_field("test", array)
+
+    sim.finish_step()
+    sim.add_field("test", 2 * array)
+
+    sim.finish_sim()
+
+    sim = temp_manager.sim(uid)
+
+    np.testing.assert_array_equal(sim.data["test"].at_step(0), array)
+    np.testing.assert_array_equal(sim.data["test"].at_step(1), 2 * array)
+
+
+def test_add_fields(temp_manager):
+    # Mock the add_field method
+    simulation_writer = temp_manager.create_simulation()
+
+    # Test data
+    fields = {
+        "field1": np.array([1, 2, 3]),
+        "field2": (np.array([4, 5, 6]), "Cell"),
+        "field3": np.array([7, 8, 9]),
+    }
+    time = 1.0
+    mesh = "test_mesh"
+
+    # Call the method
+    simulation_writer.add_fields(fields, time, mesh)
+
+    assert simulation_writer.data.keys() == fields.keys()
+    assert simulation_writer.data["field2/0"].attrs["center"] == "Cell"
+    assert simulation_writer.data["field1/0"].attrs["center"] == "Node"
+    np.testing.assert_array_equal(
+        simulation_writer.data["field3"][0], np.array([7, 8, 9])
+    )
