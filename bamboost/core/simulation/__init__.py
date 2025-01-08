3636aadfd66f48e665f99b1461fbd358a6c5f3c8
florez <florez@ethz.ch>
2025-01-08 13:44:35 +0100

    feat(sim): add Enums for FieldType (nodal, cell) and CellType (triangle, hex, ..)

--- bamboost/core/simulation/__init__.py
+++ bamboost/core/simulation/__init__.py
@@ -1,1 +1,22 @@
+from enum import Enum
+
+
+class FieldType(Enum):
+    NODE = "Node"
+    ELEMENT = "Cell"
+
+
+class CellType(Enum):
+    # 2D types
+    VERTEX = "Vertex"
+    LINE = "Line"
+    TRIANGLE = "Triangle"
+    QUAD = "Quadrilateral"
+
+    # 3D types
+    TETRAHEDRON = "Tetrahedron"
+    HEXAHEDRON = "Hexahedron"
+    WEDGE = "Wedge"
+
+
 from .base import Simulation as Simulation
